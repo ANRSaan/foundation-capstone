ca925414af1cedde32f23f7296dca81a6c97bdeb
Brian Williams <heffergod@gmail.com>
2022-09-29 13:46:15 -0700

    finished server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,24 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {SERVER_PORT} = process.env
-const {seed} = require('./controller.js')
+const {seed, getDeck, createUser, createDeck, modifyDeck, deleteDeck, deleteUser} = require('./controller.js')
 
 app.use(express.json())
 app.use(cors())
 
-
+//seed the database
 app.post('/seed', seed)
+
+//common function
+app.get('/decklist', getDeck)
+
+//users
+app.post('/users', createUser)
+app.delete('/users/:username', deleteUser)
+
+//decklists
+app.post('/decklist', createDeck)
+app.put('/decklist', modifyDeck)
+app.delete('/decklist', deleteDeck)
 
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
